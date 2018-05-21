75036b41534a0b4bbcf8367b901d7892c8eeb661
Budi Pangestu <budi.pangestu@bukalapak.com>
2018-05-21 18:34:25 +0700

    fix bug

--- lib/toggleable/configuration.rb
+++ lib/toggleable/configuration.rb
@@ -4,13 +4,13 @@ module Toggleable
     attr_accessor :redis
 
     def redis=(redis)
-      raise TypeError.new('The argument must be an instance of redis') unless valid_object?
+      raise TypeError.new('The argument must be an instance of redis') unless valid_object? redis
       self.redis = redis
     end
 
     private
 
-    def valid_object?
+    def valid_object?(redis)
       redis.is_a? Redis || redis.is_a? RedisCluster
     end
   end
