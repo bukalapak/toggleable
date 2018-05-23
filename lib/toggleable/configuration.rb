7f050dffaaa9541d0fecd13a0635f00ddbd287a7
Budi Pangestu <budi.pangestu@bukalapak.com>
2018-05-23 15:41:10 +0700

    revert the abstracts

--- lib/toggleable/configuration.rb
+++ lib/toggleable/configuration.rb
@@ -1,6 +1,6 @@
 module Toggleable
   class Configuration
-    attr_accessor :expiration_time
-    attr_accessor :redis
+    attr_accessor :expiration_time ## expiration time for memoization
+    attr_accessor :redis ## redis instance used
   end
 end
--- lib/toggleable/redis_abstract.rb
+++ lib/toggleable/redis_abstract.rb
@@ -1,11 +1,37 @@
 module Toggleable
   class RedisAbstract
-    REQUIRED_METHODS = ['get', 'set', 'hget', 'hgetall', 'hset', 'hsetnx', 'hmset', 'expire'].freeze
+    ## the redis you provide must implement these methods
 
-    REQUIRED_METHODS.each do |method|
-      define_method("#{method}") do |*args|
-        raise NotImplementedError.new("You must implement #{method}")
-      end
+    def get(_key)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def set(_key, _value, _options = {})
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def hget(_key, _field)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def hgetall(_key)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def hset(_key, _field, _value)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def hsetnx(_key, _field, _value)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def hmset(_key, *attrs)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
+    end
+
+    def expire(_key, _seconds)
+      raise NotImplementedError.new("You must implement #{__method__.to_s}")
     end
   end
 end
